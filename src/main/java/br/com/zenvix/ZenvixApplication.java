397be1af652b7a65b468f495643c8183bb7d0f29
ovitudev <maximianodev@gmail.com>
2025-10-13 19:52:56 -0300

    modified: First version for Customer Registration and Security

--- src/main/java/br/com/zenvix/ZenvixApplication.java
+++ src/main/java/br/com/zenvix/ZenvixApplication.java
@@ -2,6 +2,7 @@ package br.com.zenvix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class ZenvixApplication {
